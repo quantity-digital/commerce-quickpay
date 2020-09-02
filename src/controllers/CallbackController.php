12ef14713ed580476849416daecccae5ab9c30a2
Meldgaard <cm@quantitydigital.com>
2020-09-02 10:38:12 +0200

    fix: Fixed callbackcontroller continue function to use order returnUrl

--- src/controllers/CallbackController.php
+++ src/controllers/CallbackController.php
@@ -12,22 +12,22 @@ use yii\web\ForbiddenHttpException;
 class CallbackController extends BaseController
 {
 	/**
-     * @inheritdoc
-     */
-    public $allowAnonymous = [
-							'continue' => self::ALLOW_ANONYMOUS_LIVE | self::ALLOW_ANONYMOUS_OFFLINE,
-							'notify' => self::ALLOW_ANONYMOUS_LIVE | self::ALLOW_ANONYMOUS_OFFLINE
-						];
+	 * @inheritdoc
+	 */
+	public $allowAnonymous = [
+		'continue' => self::ALLOW_ANONYMOUS_LIVE | self::ALLOW_ANONYMOUS_OFFLINE,
+		'notify' => self::ALLOW_ANONYMOUS_LIVE | self::ALLOW_ANONYMOUS_OFFLINE
+	];
 
-    /**
-     * @inheritdoc
-     */
-    public $enableCsrfValidation = false;
+	/**
+	 * @inheritdoc
+	 */
+	public $enableCsrfValidation = false;
 
 	public function init()
-    {
-        parent::init();
-    }
+	{
+		parent::init();
+	}
 
 	/**
 	 * @param \yii\base\Action $action
@@ -45,43 +45,45 @@ class CallbackController extends BaseController
 		return true;
 	}
 
-	public function actionContinue($transactionReference = null){
+	public function actionContinue($transactionReference = null)
+	{
 		//Get transaction and order
 		$authTransaction = Plugin::getInstance()->transactions->getTransactionByHash($transactionReference);
 		$order = Plugin::getInstance()->orders->getOrderById($authTransaction->orderId);
 
 		//Order is already paid
 		if ($order->getIsPaid()) {
-			return Craft::$app->getResponse()->redirect('/shop/customer/order?number='.$order->number)->send();
+			return Craft::$app->getResponse()->redirect($order->returnUrl)->send();
 		}
 
 		// If it's successful already, we're good.
 		if (Plugin::getInstance()->getTransactions()->isTransactionSuccessful($authTransaction)) {
-			return Craft::$app->getResponse()->redirect('/shop/customer/order?number='.$order->number)->send();
+			return Craft::$app->getResponse()->redirect($order->returnUrl)->send();
 		}
 
 		//Create a new transaction with processing
-		$transaction = Plugin::getInstance()->transactions->createTransaction($order,$authTransaction);
+		$transaction = Plugin::getInstance()->transactions->createTransaction($order, $authTransaction);
 		$transaction->status = \craft\commerce\records\Transaction::STATUS_PROCESSING;
 		$transaction->type = \craft\commerce\records\Transaction::TYPE_AUTHORIZE;
 		$transaction->message = 'Authorize request completed. Waiting for final confirmation from Quickpay.';
 		Plugin::getInstance()->transactions->saveTransaction($transaction);
 
-		return Craft::$app->getResponse()->redirect('/shop/customer/order?number='.$order->number)->send();
+		return Craft::$app->getResponse()->redirect($order->returnUrl)->send();
 	}
 
-	public function actionNotify($transactionReference = null){
+	public function actionNotify($transactionReference = null)
+	{
 
-		if(!$transactionReference){
+		if (!$transactionReference) {
 			throw new ForbiddenHttpException('Missing transaction reference.');
 		}
 
-		if(!isset($_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"])){
+		if (!isset($_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"])) {
 			throw new ForbiddenHttpException('Missing Checksum.');
 		}
 
 		//Validate checksum
-		if(!$this->validateSha256Checksum($_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"])){
+		if (!$this->validateSha256Checksum($_SERVER["HTTP_QUICKPAY_CHECKSUM_SHA256"])) {
 			throw new ForbiddenHttpException('Wrong Checksum.');
 		}
 
@@ -94,9 +96,9 @@ class CallbackController extends BaseController
 		$order = Plugin::getInstance()->orders->getOrderById($authTransaction->orderId);
 
 
-		if(!$isTransactionSuccessful && $data['accepted'] && $data['state'] === 'new'){
+		if (!$isTransactionSuccessful && $data['accepted'] && $data['state'] === 'new') {
 			//Create a new transaction with processing
-			$transaction = Plugin::getInstance()->transactions->createTransaction($order,$authTransaction);
+			$transaction = Plugin::getInstance()->transactions->createTransaction($order, $authTransaction);
 			$transaction->status = \craft\commerce\records\Transaction::STATUS_SUCCESS;
 			$transaction->type = \craft\commerce\records\Transaction::TYPE_AUTHORIZE;
 			$transaction->reference = $data['id'];
@@ -106,9 +108,9 @@ class CallbackController extends BaseController
 			return;
 		}
 
-		if(!$isTransactionSuccessful && $data['state'] === 'rejected'){
+		if (!$isTransactionSuccessful && $data['state'] === 'rejected') {
 			//Create a new transaction with processing
-			$transaction = Plugin::getInstance()->transactions->createTransaction($order,$authTransaction);
+			$transaction = Plugin::getInstance()->transactions->createTransaction($order, $authTransaction);
 			$transaction->status = \craft\commerce\records\Transaction::STATUS_FAILED;
 			$transaction->type = \craft\commerce\records\Transaction::TYPE_AUTHORIZE;
 			$transaction->reference = $data['id'];
@@ -118,9 +120,9 @@ class CallbackController extends BaseController
 			return;
 		}
 
-		if(!$order->getIsPaid() && $data['state'] === 'processed'){
+		if (!$order->getIsPaid() && $data['state'] === 'processed') {
 			//Create a new transaction with processing
-			$transaction = Plugin::getInstance()->transactions->createTransaction($order,$authTransaction);
+			$transaction = Plugin::getInstance()->transactions->createTransaction($order, $authTransaction);
 			$transaction->status = \craft\commerce\records\Transaction::STATUS_SUCCESS;
 			$transaction->type = \craft\commerce\records\Transaction::TYPE_CAPTURE;
 			$transaction->reference = $data['id'];
@@ -129,7 +131,6 @@ class CallbackController extends BaseController
 			Plugin::getInstance()->transactions->saveTransaction($transaction);
 			return;
 		}
-
 	}
 
 	/**
@@ -139,10 +140,11 @@ class CallbackController extends BaseController
 	 *
 	 * @return void
 	 */
-	protected function validateSha256Checksum ($checksum){
+	protected function validateSha256Checksum($checksum)
+	{
 		$base = file_get_contents("php://input");
 		$gateway = QuickpayPlugin::$plugin->getPayments()->getGateway();
 		$privateKey = Craft::parseEnv($gateway->private_key);
-		return (hash_hmac("sha256", $base, $privateKey) === $checksum );
+		return (hash_hmac("sha256", $base, $privateKey) === $checksum);
 	}
 }
