21d9f8df8dc25ac2582d4674df63200f0658b163
Meldgaard <cm@quantity.dk>
2023-05-23 12:55:36 +0200

    Improved capture payment queue job (#5)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,22 +4,36 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 4.0.1 - 2023-05-23
+
+### Changed
+
+- `CapturePayment` QueueJob now implements `yii\queue\RetryableJobInterface` to prevent possible infinite capture loop
+
+### Added
+
+- Added new Notification e-mail setting, making it possible to receive mail if a capture job fails
+
 ## 4.0.0 - 2022-20-09
 
 ### Changed
-* Upgraded to Craft 4.0
-* Upgraded to Commerce 4.0
-* Upgraded to PHP 8.0
-* Changed selectFields to selectizedField
-* Changed lightswitchField to booleanMenu
+
+- Upgraded to Craft 4.0
+- Upgraded to Commerce 4.0
+- Upgraded to PHP 8.0
+- Changed selectFields to selectizedField
+- Changed lightswitchField to booleanMenu
 
 ### Fixed
-* Small logic changes
+
+- Small logic changes
 
 ### Added
-* Added docblocks and typing
-* Added TransactionBehavior, to centralize logic
-* Notice to README about Quickpay permissions
+
+- Added docblocks and typing
+- Added TransactionBehavior, to centralize logic
+- Notice to README about Quickpay permissions
 
 ### Deleted
-* Removed everything related to subscriptions and plans
+
+- Removed everything related to subscriptions and plans
--- src/queue/CapturePayment.php
+++ src/queue/CapturePayment.php
@@ -10,99 +10,120 @@ use craft\commerce\records\Transaction as TransactionRecord;
 use \craft\Commerce\models\Transaction;
 use \craft\Commerce\elements\Order;
 use craft\helpers\App;
+use craft\mail\Message;
+use Exception;
 use \yii\queue\Queue;
 use \yii\queue\QueueInterface;
+use yii\queue\RetryableJobInterface;
 
-class CapturePayment extends BaseJob
+class CapturePayment extends BaseJob implements RetryableJobInterface
 {
-    public Transaction $transaction;
+	public Transaction $transaction;
 
-    /**
-     * Encapsulates the Ttr
-     *
-     * @return integer
-     */
-    public function getTtr(): int
-    {
-        return 300;
-    }
+	/**
+	 * Encapsulates the Ttr
+	 *
+	 * @return integer
+	 */
+	public function getTtr(): int
+	{
+		return 60;
+	}
 
-    /**
-     * TODO: why can I not type this $queue
-     * Executes the transaction
-     *
-     * @param Queue|QueueInterface $queue
-     * @return void
-     */
-    public function execute($queue): void
-    {
-        $order = $this->transaction->order;
-        $gateway = $order->getGateway();
+	public function canRetry($attempt, $error)
+	{
+		if ($attempt == 5) {
+			$message = new Message();
+			$order = $this->transaction->order;
+			$gateway = $order->getGateway();
+			$notificationEmails = \str_replace(' ', '', $gateway->notificationEmails);
 
-        if ($order->isPaid && App::parseBooleanEnv($gateway->enableAutoStatus)) {
-            $this->updateOrderStatus($order, $gateway);
-            $this->setProgress($queue, 1);
-            return;
-        }
+			// If no notification mail is set, or notifications are disabled, we throw an exception
+			if (!$notificationEmails || !$gateway->sendNotfifications) {
+				throw new Exception($error, 503);
+			}
 
-        if (!$order->isPaid) {
-            $child = CommercePlugin::getInstance()->getPayments()->captureTransaction($this->transaction);
-            $order = $child->order;
-            $this->setProgress($queue, .5);
+			$message->setTo($notificationEmails);
+			$message->setSubject('Capture payment failed');
+			$message->setTextBody("Capture payment for order {$order->reference} failed.");
 
-            if ($child->status === TransactionRecord::STATUS_SUCCESS) {
-                $order->updateOrderPaidInformation();
-                if (App::parseBooleanEnv($gateway->enableAutoStatus)) {
-                    $this->updateOrderStatus($order, $gateway);
-                }
-            } else {
-                $this->reAddToQueue();
-            }
-        }
+			// Swallow exceptions from the mailer:
+			try {
+				Craft::$app->getMailer()->send($message);
+			} catch (\Throwable $e) {
+				Craft::warning("Something went wrong: {$e->getMessage()}", __METHOD__);
+			}
 
-        $this->setProgress($queue, 1);
-    }
+			// Throw an exception to retry the job later
+			throw new Exception($error, 503);
+		}
 
-    // Protected Methods
-    // =========================================================================
+		return ($attempt < 5);
+	}
 
-    /**
-     * Encapsulates the default description
-     *
-     * @return string
-     */
-    protected function defaultDescription(): string
-    {
-        return 'Capture quickpay payment';
-    }
+	/**
+	 * Executes the transaction
+	 *
+	 * @param Queue|QueueInterface $queue
+	 * @return void
+	 */
+	public function execute($queue): void
+	{
+		$order = $this->transaction->order;
+		$gateway = $order->getGateway();
 
-    /**
-     * Adds the transaction to the queue, after a 300 ms delay
-     *
-     * @return void
-     */
-    protected function reAddToQueue(): void
-    {
-        Craft::$app->getQueue()->delay(300)->push(new CapturePayment(
-            [
-                'transaction' => $this->transaction,
-            ]
-        ));
-    }
+		//Order is already paid, so we can just update the status
+		if ($order->isPaid && App::parseBooleanEnv($gateway->enableAutoStatus)) {
+			$this->updateOrderStatus($order, $gateway);
+			$this->setProgress($queue, 1);
+			return;
+		}
 
-    /**
-     * Updates order status
-     *
-     * @param Order $order to update
-     * @param Gateway $gateway
-     * @return void
-     * @throws Throwable
-     */
-    protected function updateOrderStatus(Order $order, BaseGatewayInterface $gateway): void
-    {
-        $orderStatus = CommercePlugin::getInstance()->getOrderStatuses()->getOrderStatusByHandle(App::parseEnv($gateway->afterCaptureStatus));
-        $order->orderStatusId = $orderStatus->id;
+		//Order is not paid, so we need to capture the transaction
+		if (!$order->isPaid) {
+			$child = CommercePlugin::getInstance()->getPayments()->captureTransaction($this->transaction);
+			$order = $child->order;
+			$this->setProgress($queue, .5);
 
-        Craft::$app->getElements()->saveElement($order);
-    }
+			if ($child->status === TransactionRecord::STATUS_SUCCESS) {
+				$order->updateOrderPaidInformation();
+				if (App::parseBooleanEnv($gateway->enableAutoStatus)) {
+					$this->updateOrderStatus($order, $gateway);
+				}
+			} else {
+				throw new Exception('Could not capture payment');
+			}
+		}
+
+		$this->setProgress($queue, 1);
+	}
+
+	// Protected Methods
+	// =========================================================================
+
+	/**
+	 * Encapsulates the default description
+	 *
+	 * @return string
+	 */
+	protected function defaultDescription(): string
+	{
+		return 'Capture quickpay payment';
+	}
+
+	/**
+	 * Updates order status
+	 *
+	 * @param Order $order to update
+	 * @param Gateway $gateway
+	 * @return void
+	 * @throws Throwable
+	 */
+	protected function updateOrderStatus(Order $order, BaseGatewayInterface $gateway): void
+	{
+		$orderStatus = CommercePlugin::getInstance()->getOrderStatuses()->getOrderStatusByHandle(App::parseEnv($gateway->afterCaptureStatus));
+		$order->orderStatusId = $orderStatus->id;
+
+		Craft::$app->getElements()->saveElement($order);
+	}
 }
